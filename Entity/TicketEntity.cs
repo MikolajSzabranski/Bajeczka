778e2c45cffafd08e5cd642ebed8738b12f49e86
Mikołaj Szabrański <mikolaj.szabranski@student.wat.edu.pl>
2024-01-31 13:14:19 +0100

    pod wyświetlanie na potem

--- Entity/TicketEntity.cs
+++ Entity/TicketEntity.cs
@@ -10,14 +10,14 @@ public class TicketEntity
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     [Column("biletid")]
     public int Id { get; set; }
-    //
-    // [Column("datazakupu")]
-    // [DataType(DataType.DateTime)]
-    // public DateTime PurchaseDate { get; set; }
-    //
-    // [Column("seansid")]   
-    // public int ShowId { get; set; }
-    //
-    // [Column("uzytkownikid")]
-    // public int UserId { get; set; }
+    
+    [Column("datazakupu")]
+    [DataType(DataType.DateTime)]
+    public DateTime PurchaseDate { get; set; }
+    
+	[Column("seansid")]   
+    public int ShowtimeId { get; set; }
+    
+    [Column("uzytkownikid")]
+    public int UserId { get; set; }
 }
